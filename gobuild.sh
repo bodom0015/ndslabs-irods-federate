9469d4813c2fe6eab52ac270bdc4e124a54a6381
Craig Willis <willis8@illinois.edu>
2016-06-07 10:55:54 -0500

    Added test data, updated build script

--- gobuild.sh
+++ gobuild.sh
@@ -2,4 +2,4 @@
 
 cd /go/src/github.com/ndslabs-irods-federate
 go get
-GOOS=linux GOARCH=amd64 go build -o build/bin/amd64/ndslabs-irods-federate
+GOOS=linux GOARCH=amd64 go build -o build/bin/ndslabs-irods-federate-linux-amd64
